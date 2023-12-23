44962fc2afca1edc694ed7353e28dd228a09edaa
Nathz <triputraadityaaditya@gmail.com>
2023-12-23 13:25:27 +0700

    Upload File c3

---
+++ praktikum-adit-c3.py
@@ -0,0 +1,52 @@
+
+
+
+print('PRAKTIKUM 3')
+print()
+nama='tri putra aditya'
+nim='231031084'
+meet='Praktikum-3-(String)'
+prodi='Sistem-Informasi-C'
+email='triputraadityaaditya@gmail.com'
+
+sp = 133
+print('-'*sp)
+print(nama.center(sp))
+print(nim.center(sp))
+print('\n'*2)
+print(meet.rjust(sp))
+print(prodi.rjust(sp))
+print(email.lower().rjust(sp))
+print('-'*sp)
+
+sp = 20
+a = 'Halo, nama saya'
+b = 'dengan NIM'
+c = 'dari prodi'
+d = ', ini adalah file'
+e = '. Terima kasih'
+print((a).rjust(sp),(nama),(b),(nim),(c),(prodi),(d),(meet),(e) )
+print()
+Kota = 'Bone'
+ttl = '22-12-2004'
+alamat = 'Jl. Kelapa Gading'
+asal = 'Bone'
+hobi = 'Futsal'
+tinggi = '162'
+
+
+print (f'''Biodata saya:
+Nama\t: {nama}
+NIM\t: {nim}
+Prodi\t: {prodi}
+TTL\t: {ttl}
+Alamat\t: {alamat}
+Asal\t: {asal}
+Hobi\t: {hobi}
+Tinggi\t: {tinggi}cm
+''')
+
+
+
+
+
